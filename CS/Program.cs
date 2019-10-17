e7d9c0dc74b37090683e831cfeb281a6ce650c82
Eliodor Popov <eliodor.popov@outlook.com>
2019-10-17 15:42:44 +0300

    adding proj

---
+++ CS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+namespace CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to encrypt:");
+            string stringToEncrypt = Console.ReadLine();
+            Console.WriteLine("Encrypted string: " + GetEncryptedStringDES(stringToEncrypt));
+            Console.WriteLine("Encrypted string AES: " + GetEncryptedStringAES(stringToEncrypt));
+            Console.Read();
+        }
+        public static string GetEncryptedStringDES(string textToEncrypt)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using ( StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write("dfklghsdljfgh;sdljfsdfdfgfdgdfgdfg");
+                        sw.Flush();
+                        cs.FlushFinalBlock();
+                        sw.Flush();
+                        return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+                    }
+                }
+            }
+        }
+        public static string GetEncryptedStringAES(string textToEncrypt)
+        {
+            AesManaged aes = new AesManaged();
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(textToEncrypt);
+                        sw.Flush();
+                        cs.FlushFinalBlock();
+                        sw.Flush();
+                        return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+                    }
+                }
+            }
+        }
+    }
+}
